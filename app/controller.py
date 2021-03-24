a09c3e7f773891ae3c54dd6f488a7ec8300aab1c
Addie Liu <liuaddie@gmail.com>
2021-03-24 20:34:42 +0800

    u

--- app/controller.py
+++ app/controller.py
@@ -701,26 +701,26 @@ class Device:
         # check_ssid = subprocess.Popen(["networksetup","-getairportnetwork","en0"], stdout=subprocess.PIPE)
         # nmcli -a d wifi connect DIRECT-yRE0:NEX-5R password fWc7xbLM
         # print(check_ssid_result)
-        # print(self.get('cam_ssid'))
+        print(self.get('cam_ssid'))
         if check_ssid_result == self.get('cam_ssid'):
             print("SSID is correct")
             # check_ssid = subprocess.Popen(["networksetup","-getairportnetwork","en0"], stdout=subprocess.PIPE)
             # if 192.168.122.1:8080
-            check_ping = subprocess.Popen(["ping",self.get('cam_ip').split(":")[0],"-c","2","-W","1000"], stdout=subprocess.PIPE)
-            # stdout = process.communicate()[0]
-            timeout = False
-            while True:
-                output = check_ping.stdout.readline().decode("utf-8").strip()
-                if output == '' and check_ping.poll() is not None:
-                    break
-                if output:
-                    try:
-                        # print(output)
-                        if output.split(" ")[1] == "timeout":
-                            timeout = True
-                    except:
-                        pass
-            check_ping_result = check_ping.poll()
+            # check_ping = subprocess.Popen(["ping",self.get('cam_ip').split(":")[0],"-c","2","-W","1000"], stdout=subprocess.PIPE)
+            # # stdout = process.communicate()[0]
+            # timeout = False
+            # while True:
+            #     output = check_ping.stdout.readline().decode("utf-8").strip()
+            #     if output == '' and check_ping.poll() is not None:
+            #         break
+            #     if output:
+            #         try:
+            #             # print(output)
+            #             if output.split(" ")[1] == "timeout":
+            #                 timeout = True
+            #         except:
+            #             pass
+            # check_ping_result = check_ping.poll()
             # check_ping.wait()
             # check_ping_result = check_ping.communicate()[0].decode("utf-8")
             # print(check_ping_result)
