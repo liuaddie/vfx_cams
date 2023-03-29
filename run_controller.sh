b76da9b9581d1ee52636bf3b819f20baaae49fa5
Addie Liu <liuaddie@gmail.com>
2023-03-29 23:04:03 +0800

    Update run_controller.sh

--- run_controller.sh
+++ run_controller.sh
@@ -1,4 +1,8 @@
-#1/bin/bash
+#!/bin/bash
 
-cd /root/repo/adp_sony_remote/app/
+cd /root/repo/vfx_cams/
+git pull origin master
+source venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
 python3 controller.py
