258399733f481745ab8aa8da3b834c4ee38b12c9
Addie Liu <liuaddie@gmail.com>
2023-04-08 09:28:16 +0800

    Update controller.py

--- controller.py
+++ controller.py
@@ -148,22 +148,22 @@ if f:
                                     fn = getattr(s, action)
                                     rs = fn()
                                     done = True
-                                    if action == "actTakePicture":
-                                        print(rs['result'][0][0])
-                                        url = rs['result'][0][0].replace("\\", "")
-                                        response = requests.get(url)
-                                        if url.find('/'):
-                                            filename = "{}_{}_{}".format(tt, d.get('id'), url.rsplit('/', 1)[1])
-                                            print(filename)
-                                        src_folder = "_photogrammetry_src"
-                                        tt_folder = "{}/{}".format(src_folder, tt)
-                                        if not os.path.exists(src_folder):
-                                            os.makedirs(src_folder)
-                                        if not os.path.exists(tt_folder):
-                                            os.makedirs(tt_folder)
-
-                                        filepath = '{}/{}'.format(tt_folder,filename)
-                                        open(filepath, "wb").write(response.content)
+                                    # if action == "actTakePicture":
+                                    #     print(rs['result'][0][0])
+                                    #     url = rs['result'][0][0].replace("\\", "")
+                                    #     response = requests.get(url)
+                                    #     if url.find('/'):
+                                    #         filename = "{}_{}_{}".format(tt, d.get('id'), url.rsplit('/', 1)[1])
+                                    #         print(filename)
+                                    #     src_folder = "_photogrammetry_src"
+                                    #     tt_folder = "{}/{}".format(src_folder, tt)
+                                    #     if not os.path.exists(src_folder):
+                                    #         os.makedirs(src_folder)
+                                    #     if not os.path.exists(tt_folder):
+                                    #         os.makedirs(tt_folder)
+                                    #
+                                    #     filepath = '{}/{}'.format(tt_folder,filename)
+                                    #     open(filepath, "wb").write(response.content)
 
                                         # thread_ftp = ThreadWithResult(target=upload, args=(filepath,tt_folder,filename))
                                         # thread_ftp.start()
@@ -266,7 +266,7 @@ def connect():
     e_chk = 0
     while True:
         try:
-            time.sleep(10)
+            time.sleep(20)
             e_chk += 1
             print("*"*23)
             print("** e: {} {} ".format(e(), e_chk))
